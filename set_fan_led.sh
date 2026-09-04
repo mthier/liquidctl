ea557013069a2b5a5c786f028d8a0e519ca85645
Michael Thiermann <thiermann.michael@gmail.com>
2026-09-04 10:10:08 +0200

    Add new color

--- set_fan_led.sh
+++ set_fan_led.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller Kraken" set led1 color fixed ff0067
-sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller Kraken" set led2 color fixed ff00c6
-sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller F-Series" set led1 color fixed ff2200
-sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller F-Series" set led2 color fixed ff1000
-sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller F-Series" set led3 color fixed e400ff
+sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller Kraken" set led1 color fixed ff322d
+sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller Kraken" set led2 color fixed ff322d
+sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller F-Series" set led1 color fixed ff322d
+sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller F-Series" set led2 color fixed ff322d
+sudo /opt/liquidctl/venv/bin/python3 -m liquidctl --match "NZXT RGB Controller F-Series" set led3 color fixed ff322d
